ade290c6eaf77c69f6cd7c313c091cc4a8cfe01c
DhruvPatel-30 <Dpatel2297@conestogac.on.ca>
2025-11-26 19:30:22 -0500

    update lamda message

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello This is my CDK Lambda for AWS CodePipeline!" }),
+    body: JSON.stringify({ message: "Updated message!" }),
   };
 };
